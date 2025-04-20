8233fc64ee270f14c68ecfa6dd9620d43d8de5fd
DM <digimabble@gmail.com>
2025-04-20 20:16:58 +0200

    logo change

--- src/context/constants.ts
+++ src/context/constants.ts
@@ -4,17 +4,17 @@ export const currency: CurrencyType = '$'
 
 export const currentYear = new Date().getFullYear()
 
-export const developedByLink = 'https://techzaa.getappui.com/'
+export const developedByLink = 'https://digimabble.com/'
 
-export const developedBy = 'Techzaa'
+export const developedBy = 'digimabble'
 
-export const contactUs = 'techzaastudio@gmail.com'
+export const contactUs = 'digimabble@gmail.com'
 
 export const buyLink = ''
 
 export const basePath = ''
 
-export const DEFAULT_PAGE_TITLE = 'Lahomes | Responsive Admin Dashboard Template'
+export const DEFAULT_PAGE_TITLE = 'La Regence | AI  Admin Dashboard'
 
 // Replace the URL's value in env with your backend's URL or if you're using nextjs's API, add the server's origin URL
 export const API_BASE_PATH = ''
